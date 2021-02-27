015e2c0bac24819e2f41ce861152549f81b18dbf
Nicolas Bustamante <bustamante.n.f@gmail.com>
2021-02-27 13:41:19 -0300

    add local query scope order by creation date desc

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function scopebyCreationDateDesc($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
